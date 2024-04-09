20ff63f56cf8ed60b476b68c6c91654e625e7fed
Ahmed Amin <ahmedamin100dev@gmail.com>
2024-04-09 14:21:31 +0200

    Update CartStore.jsx to use named import for create from zustand

--- src/store/CartStore.jsx
+++ src/store/CartStore.jsx
@@ -1,1 +1,9 @@
-import create from "zustand";
+import { create } from "zustand";
+
+export const useCartStore = create((set) => ({
+  cart: [],
+  addToCart: (product) => set((state) => ({ cart: [...state.cart, product] })),
+  removeFromCart: (productId) =>
+    set((state) => ({ cart: state.cart.filter((p) => p.id !== productId) })),
+  clearCart: () => set({ cart: [] }),
+}));
